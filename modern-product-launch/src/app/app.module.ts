0b316d312faecc7f8d5741959b415ba8ff39c505
Arif Umrani <itsarifumrani@gmail.com>
2026-05-18 15:27:24 +0500

    chore(home): routing path set

--- modern-product-launch/src/app/app.module.ts
+++ modern-product-launch/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BenefitsComponent } from './pages/benefits/benefits.component';
+// import { BenefitsComponent } from './pages/benefits/benefits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    BenefitsComponent
+    // BenefitsComponent
   ],
   imports: [
     BrowserModule,
